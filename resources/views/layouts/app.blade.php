259db377496b8de4aa60e4d4b2b8ef12a4375632
frizehero <bag4sk4r4@gmail.com>
2025-02-24 23:06:45 +0700

    feat: setup template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <!-- --------------------------------------------------- -->
+    <!-- Title -->
+    <!-- --------------------------------------------------- -->
+    <title>Mordenize</title>
+    <!-- --------------------------------------------------- -->
+    <!-- Required Meta Tag -->
+    <!-- --------------------------------------------------- -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="handheldfriendly" content="true" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="description" content="Mordenize" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="Mordenize" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- --------------------------------------------------- -->
+    <!-- Favicon -->
+    <!-- --------------------------------------------------- -->
+    <link rel="shortcut icon" type="image/png" href="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" />
+    <!-- --------------------------------------------------- -->
+    <!-- Core Css -->
+    <!-- --------------------------------------------------- -->
+    <link  id="themeColors"  rel="stylesheet" href="{{ asset('assets') }}/css/style.min.css" />
+  </head>
+  <body>
+    <!-- Preloader -->
+    <div class="preloader">
+      <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" alt="loader" class="lds-ripple img-fluid" />
+    </div>
+    <!-- --------------------------------------------------- -->
+    <!-- Body Wrapper -->
+    <!-- --------------------------------------------------- -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
+      <!-- --------------------------------------------------- -->
+      <!-- Sidebar -->
+      <!-- --------------------------------------------------- -->
+      @include('layouts.sidebar')
+      <!-- --------------------------------------------------- -->
+      <!-- Main Wrapper -->
+      <!-- --------------------------------------------------- -->
+      <div class="body-wrapper">
+        <!-- --------------------------------------------------- -->
+        <!-- Header Start -->
+        <!-- --------------------------------------------------- -->
+        @include('layouts.header')
+        <!-- --------------------------------------------------- -->
+        <!-- Header End -->
+        <!-- --------------------------------------------------- -->
+        @yield('content')
+      </div>
+    </div>
+    <!--  Mobilenavbar -->
+    @include('layouts.mobileNavbar')
+    <!-- ---------------------------------------------- -->
+    <!-- Import Js Files -->
+    <!-- ---------------------------------------------- -->
+    <script src="{{ asset('assets') }}/libs/jquery/dist/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/simplebar/dist/simplebar.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- ---------------------------------------------- -->
+    <!-- core files -->
+    <!-- ---------------------------------------------- -->
+    <script src="{{ asset('assets') }}/js/app.min.js"></script>
+    <script src="{{ asset('assets') }}/js/app.init.js"></script>
+    <script src="{{ asset('assets') }}/js/app-style-switcher.js"></script>
+    <script src="{{ asset('assets') }}/js/sidebarmenu.js"></script>
+    
+    <script src="{{ asset('assets') }}/js/custom.js"></script>
+    <!-- ---------------------------------------------- -->
+    <!-- current page js files -->
+    <!-- ---------------------------------------------- -->
+    <script src="{{ asset('assets') }}/js/apps/chat.js"></script>
+  </body>
+
+<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/blog-posts.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 06 Jul 2023 01:59:25 GMT -->
+</html>
