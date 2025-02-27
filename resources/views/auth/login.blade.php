9e6c8eacd4ad9a4df22091f30fd7473f021b611c
frizehero <bag4sk4r4@gmail.com>
2025-02-27 20:22:20 +0700

    feat: setup template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,26 +44,8 @@
             <div class="col-xl-5 col-xxl-4">
               <div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
                 <div class="col-sm-8 col-md-6 col-xl-9">
-                  <h2 class="mb-3 fs-7 fw-bolder">Welcome to Modernize</h2>
-                  <p class=" mb-9">Your Admin Dashboard</p>
-                  <div class="row">
-                    <div class="col-6 mb-2 mb-sm-0">
-                      <a class="btn btn-white text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8" href="javascript:void(0)" role="button">
-                        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/google-icon.svg" alt="" class="img-fluid me-2" width="18" height="18">
-                        <span class="d-none d-sm-block me-1 flex-shrink-0">Sign in with</span>Google
-                      </a>
-                    </div>
-                    <div class="col-6">
-                      <a class="btn btn-white text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8" href="javascript:void(0)" role="button">
-                        <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/facebook-icon.svg" alt="" class="img-fluid me-2" width="18" height="18">
-                        <span class="d-none d-sm-block me-1 flex-shrink-0">Sign in with</span>FB
-                      </a>
-                    </div>
-                  </div>
-                  <div class="position-relative text-center my-4">
-                    <p class="mb-0 fs-4 px-3 d-inline-block bg-white text-dark z-index-5 position-relative">or sign in with</p>
-                    <span class="border-top w-100 position-absolute top-50 start-50 translate-middle"></span>
-                  </div>
+                  <h2 class="mb-3 fs-7 fw-bolder">Meubel Aji</h2>
+                  <p class=" mb-9">Silakan masuk dengan akun yang sudah terdaftar</p>
                   <form>
                     <div class="mb-3">
                       <label for="exampleInputEmail1" class="form-label">Username</label>
@@ -85,7 +67,7 @@
                     <a href="index-2.html" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign In</a>
                     <div class="d-flex align-items-center justify-content-center">
                       <p class="fs-4 mb-0 fw-medium">New to Modernize?</p>
-                      <a class="text-primary fw-medium ms-2" href="authentication-register.html">Create an account</a>
+                      <a class="text-primary fw-medium ms-2" href="authentication-register.html">Belum punya akun</a>
                     </div>
                   </form>
                 </div>
