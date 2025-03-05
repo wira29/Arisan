7fe80a5c2d49f0510d4294c457fa3afc68cb9371
frizehero <bag4sk4r4@gmail.com>
2025-03-06 01:43:52 +0700

    mod: update migration

--- database/migrations/2025_02_27_145236_add_phone_number_in_user.php
+++ database/migrations/2025_02_27_145236_add_phone_number_in_user.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('phone_number')->after('email');
         });
     }
